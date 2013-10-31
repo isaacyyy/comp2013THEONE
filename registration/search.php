620ea88493075a50f7abe560992b7fa98fa931d5
root <root@studvm52-p.cs.ucl.ac.uk>
2013-10-31 12:00:25 +0000

    changed search to stop sql injection

--- registration/search.php
+++ registration/search.php
@@ -61,7 +61,7 @@
     $sql_select = "SELECT * FROM registration_tbl WHERE name LIKE '%".?."%'";
     $stmt = $conn->prepare($sql_select);
     $stmt->bindValue(1, $name);
-    $stmt->execute();
+    $stmt->execute($sql_select);
     $registrants = $stmt->fetchAll();
     if(count($registrants) > 0) {
         echo "<h2>Search Results:</h2>";
