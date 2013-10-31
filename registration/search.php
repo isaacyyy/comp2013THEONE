75836008f957a9aafec19835650c5c94a2bbccae
root <root@studvm52-p.cs.ucl.ac.uk>
2013-10-31 11:53:05 +0000

    changed search to stop sql injection

--- registration/search.php
+++ registration/search.php
@@ -58,12 +58,11 @@
     echo "<h3>Your Search Results!</h3>";
     }
     // Retrieve data
-    /*$sql_select = "SELECT * FROM registration_tbl WHERE name LIKE '%".?."%'";
-    $stmt->bindParam("?", $name);
-    $stmt = $conn->query($sql_select);
-    $registrants = $stmt->fetchAll();*/
-    $stmt = $dbh->prepare("SELECT * FROM registration_tbl WHERE name LIKE '%".?."%'";
-    $stmt->(execute($name));
+    $sql_select = "SELECT * FROM registration_tbl WHERE name LIKE '%".?."%'";
+    $stmt = $conn->prepare($sql_select);
+    $stmt->bindValue("1", $name);
+    $stmt->execute();
+    $registrants = $stmt->fetchAll();
     if(count($registrants) > 0) {
         echo "<h2>Search Results:</h2>";
         echo "<table>";
